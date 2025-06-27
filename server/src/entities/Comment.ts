e930345770af1db6888ce54c4b319e50aa484c5a
LUDOVIC SCELLES <ludovic.scelles@gmail.com>
2025-06-27 08:13:49 +0200

    feat(data-type-validation): validate types with typeorm and class-validator

--- server/src/entities/Comment.ts
+++ server/src/entities/Comment.ts
@@ -2,19 +2,28 @@ import "reflect-metadata";
 import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from "typeorm";
 import { Snippet } from "./Snippet";
 import { User } from "./User";
+import { IsString, Length, IsNotEmpty, IsDate } from "class-validator";
 
 @Entity()
 export class Comment {
   @PrimaryGeneratedColumn()
   id!: number;
 
-  @Column("text")
+  @Column({ nullable: false, type: "text" })
+  @IsString()
+  @IsNotEmpty()
+  @Length(1, 5000)
   suggestedCode!: string;
 
-  @Column("text")
+  @Column({nullable: false, type: "text" })
+  @IsString()
+  @IsNotEmpty()
+  @Length(1, 5000)
   message!: string;
 
   @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP" })
+  @IsNotEmpty()
+  @IsDate()
   createdAt!: Date;
 
   @ManyToOne(() => Snippet, (snippet) => snippet.comments)
--- server/src/entities/Snippet.ts
+++ server/src/entities/Snippet.ts
@@ -1,29 +1,49 @@
 import "reflect-metadata";
-import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, OneToMany } from "typeorm";
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  OneToMany,
+} from "typeorm";
 import { User } from "./User";
 import { Comment } from "./Comment";
+import { IsString, Length, IsNotEmpty, IsDate } from "class-validator";
 
 @Entity()
 export class Snippet {
   @PrimaryGeneratedColumn()
   id!: number;
 
-  @Column()
+  @Column({ nullable: false, type: "varchar", length: 150 })
+  @IsString()
+  @IsNotEmpty()
+  @Length(1, 150)
   title!: string;
 
-  @Column("text")
+  @Column({ nullable: false, type: "text"})
+  @IsString()
+  @IsNotEmpty()
+  @Length(1, 5000)
   code!: string;
 
-  @Column("text")
+  @Column({ nullable: false, type: "text" })
+  @IsString()
+  @IsNotEmpty()
+  @Length(1, 5000)
   message!: string;
 
   @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP" })
+  @IsNotEmpty()
+  @IsDate()
   createdAt!: Date;
 
-  @ManyToOne(() => User, (user) => user.snippets, { nullable: false, onDelete: "CASCADE" })
+  @ManyToOne(() => User, (user) => user.snippets, {
+    nullable: false,
+    onDelete: "CASCADE",
+  })
   user!: User;
 
   @OneToMany(() => Comment, (comment) => comment.snippet)
   comments!: Comment[];
-
 }

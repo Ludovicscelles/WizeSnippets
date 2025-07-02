cd56c8c46405f22acfd5e4878ae04a9779f3a7c4
LUDOVIC SCELLES <ludovic.scelles@gmail.com>
2025-07-02 20:29:58 +0200

    unitest(frontend): add unit tests for button component

---
+++ vitest.config.server.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test : {
+    globals: true,
+    environment: "node",
+    setupFiles: ["./server/setupTest.ts"],
+    include: ["server/test/*.ts"]
+  },
+});

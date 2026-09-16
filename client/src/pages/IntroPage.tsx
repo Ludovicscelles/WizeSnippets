7ebb48411a3a5e87b96f8c31fa9a848cf0d3fc26
Ludovicscelles <161328869+Ludovicscelles@users.noreply.github.com>
2026-09-16 16:25:22 +0200

    feat(introPage): add navigation button and responsive text with tailwind

--- client/src/pages/IntroPage.tsx
+++ client/src/pages/IntroPage.tsx
@@ -1,7 +1,6 @@
 import logo from "../assets/logo.svg";
 import mouse from "../assets/mouse.svg";
 import { useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
 
 export default function IntroPage() {
   const navigate = useNavigate();
@@ -9,20 +8,6 @@ export default function IntroPage() {
   const handleClick = () => {
     navigate("/snippets");
   };
-
-  const [isMobile, setIsMobile] = useState(false);
-  useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <= 768);
-    };
-
-    handleResize();
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
 
   return (
     <div className="flex w-full min-h-screen bg-black items-center justify-center md:items-start md:justify-center overflow-hidden">
@@ -35,13 +20,15 @@ export default function IntroPage() {
         <h1 className="text-white text-[4rem] font-bold mb-10 max-w-xs md:max-w-none md:text-[6rem]">
           Partage ton code&nbsp;!
         </h1>
-        <h2
+        <button
+          type="button"
           onClick={handleClick}
           className="text-white flex items-center text-[2rem] gap-4 font-bold mb-10 md:text-[3rem]"
         >
-          {isMobile ? "Entre ici" : "Clique ici"}
+          <span className="md:hidden">Entre ici</span>
+          <span className="hidden md:inline">Clique ici pour commencer</span>
           <img src={mouse} className="w-8 h-8" alt="mouse" />
-        </h2>
+        </button>
       </div>
     </div>
   );
